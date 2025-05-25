06eccd6e3b01e613b6d76879c023dfc3ecabe550
MarceloGToonz <bendygalvao@gmail.com>
2025-05-25 00:02:08 -0300

    wuh

--- src/main/java/net/mcreator/yafnafmod/init/YaFnafmodModMenus.java
+++ src/main/java/net/mcreator/yafnafmod/init/YaFnafmodModMenus.java
@@ -12,18 +12,11 @@ import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraft.world.inventory.MenuType;
 
 import net.mcreator.yafnafmod.world.inventory.WallCreatorMenu;
-import net.mcreator.yafnafmod.world.inventory.WackyWartPasteAdMenu;
-import net.mcreator.yafnafmod.world.inventory.PeppersPickledPopcornAdMenu;
 import net.mcreator.yafnafmod.world.inventory.MonitorWorkGuiMenu;
-import net.mcreator.yafnafmod.world.inventory.MartysPlungersAdMenu;
 import net.mcreator.yafnafmod.world.inventory.LockerGuiMenu;
-import net.mcreator.yafnafmod.world.inventory.LallysLolliesAdMenu;
 import net.mcreator.yafnafmod.world.inventory.FridgeIndustrialGUIMenu;
 import net.mcreator.yafnafmod.world.inventory.FridgeGUIMenu;
-import net.mcreator.yafnafmod.world.inventory.FlosFlossyFlossAdMenu;
-import net.mcreator.yafnafmod.world.inventory.FiztimeSodaPopAdMenu;
 import net.mcreator.yafnafmod.world.inventory.FatLetterUiMenu;
-import net.mcreator.yafnafmod.world.inventory.ElChipAdMenu;
 import net.mcreator.yafnafmod.world.inventory.CabinetMenu;
 import net.mcreator.yafnafmod.YaFnafmodMod;
 
@@ -36,11 +29,4 @@ public class YaFnafmodModMenus {
 	public static final RegistryObject<MenuType<FatLetterUiMenu>> FAT_LETTER_UI = REGISTRY.register("fat_letter_ui", () -> IForgeMenuType.create(FatLetterUiMenu::new));
 	public static final RegistryObject<MenuType<FridgeGUIMenu>> FRIDGE_GUI = REGISTRY.register("fridge_gui", () -> IForgeMenuType.create(FridgeGUIMenu::new));
 	public static final RegistryObject<MenuType<FridgeIndustrialGUIMenu>> FRIDGE_INDUSTRIAL_GUI = REGISTRY.register("fridge_industrial_gui", () -> IForgeMenuType.create(FridgeIndustrialGUIMenu::new));
-	public static final RegistryObject<MenuType<FiztimeSodaPopAdMenu>> FIZTIME_SODA_POP_AD = REGISTRY.register("fiztime_soda_pop_ad", () -> IForgeMenuType.create(FiztimeSodaPopAdMenu::new));
-	public static final RegistryObject<MenuType<LallysLolliesAdMenu>> LALLYS_LOLLIES_AD = REGISTRY.register("lallys_lollies_ad", () -> IForgeMenuType.create(LallysLolliesAdMenu::new));
-	public static final RegistryObject<MenuType<FlosFlossyFlossAdMenu>> FLOS_FLOSSY_FLOSS_AD = REGISTRY.register("flos_flossy_floss_ad", () -> IForgeMenuType.create(FlosFlossyFlossAdMenu::new));
-	public static final RegistryObject<MenuType<WackyWartPasteAdMenu>> WACKY_WART_PASTE_AD = REGISTRY.register("wacky_wart_paste_ad", () -> IForgeMenuType.create(WackyWartPasteAdMenu::new));
-	public static final RegistryObject<MenuType<MartysPlungersAdMenu>> MARTYS_PLUNGERS_AD = REGISTRY.register("martys_plungers_ad", () -> IForgeMenuType.create(MartysPlungersAdMenu::new));
-	public static final RegistryObject<MenuType<ElChipAdMenu>> EL_CHIP_AD = REGISTRY.register("el_chip_ad", () -> IForgeMenuType.create(ElChipAdMenu::new));
-	public static final RegistryObject<MenuType<PeppersPickledPopcornAdMenu>> PEPPERS_PICKLED_POPCORN_AD = REGISTRY.register("peppers_pickled_popcorn_ad", () -> IForgeMenuType.create(PeppersPickledPopcornAdMenu::new));
 }
--- src/main/java/net/mcreator/yafnafmod/init/YaFnafmodModScreens.java
+++ src/main/java/net/mcreator/yafnafmod/init/YaFnafmodModScreens.java
@@ -12,18 +12,11 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraft.client.gui.screens.MenuScreens;
 
 import net.mcreator.yafnafmod.client.gui.WallCreatorScreen;
-import net.mcreator.yafnafmod.client.gui.WackyWartPasteAdScreen;
-import net.mcreator.yafnafmod.client.gui.PeppersPickledPopcornAdScreen;
 import net.mcreator.yafnafmod.client.gui.MonitorWorkGuiScreen;
-import net.mcreator.yafnafmod.client.gui.MartysPlungersAdScreen;
 import net.mcreator.yafnafmod.client.gui.LockerGuiScreen;
-import net.mcreator.yafnafmod.client.gui.LallysLolliesAdScreen;
 import net.mcreator.yafnafmod.client.gui.FridgeIndustrialGUIScreen;
 import net.mcreator.yafnafmod.client.gui.FridgeGUIScreen;
-import net.mcreator.yafnafmod.client.gui.FlosFlossyFlossAdScreen;
-import net.mcreator.yafnafmod.client.gui.FiztimeSodaPopAdScreen;
 import net.mcreator.yafnafmod.client.gui.FatLetterUiScreen;
-import net.mcreator.yafnafmod.client.gui.ElChipAdScreen;
 import net.mcreator.yafnafmod.client.gui.CabinetScreen;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -38,13 +31,6 @@ public class YaFnafmodModScreens {
 			MenuScreens.register(YaFnafmodModMenus.FAT_LETTER_UI.get(), FatLetterUiScreen::new);
 			MenuScreens.register(YaFnafmodModMenus.FRIDGE_GUI.get(), FridgeGUIScreen::new);
 			MenuScreens.register(YaFnafmodModMenus.FRIDGE_INDUSTRIAL_GUI.get(), FridgeIndustrialGUIScreen::new);
-			MenuScreens.register(YaFnafmodModMenus.FIZTIME_SODA_POP_AD.get(), FiztimeSodaPopAdScreen::new);
-			MenuScreens.register(YaFnafmodModMenus.LALLYS_LOLLIES_AD.get(), LallysLolliesAdScreen::new);
-			MenuScreens.register(YaFnafmodModMenus.FLOS_FLOSSY_FLOSS_AD.get(), FlosFlossyFlossAdScreen::new);
-			MenuScreens.register(YaFnafmodModMenus.WACKY_WART_PASTE_AD.get(), WackyWartPasteAdScreen::new);
-			MenuScreens.register(YaFnafmodModMenus.MARTYS_PLUNGERS_AD.get(), MartysPlungersAdScreen::new);
-			MenuScreens.register(YaFnafmodModMenus.EL_CHIP_AD.get(), ElChipAdScreen::new);
-			MenuScreens.register(YaFnafmodModMenus.PEPPERS_PICKLED_POPCORN_AD.get(), PeppersPickledPopcornAdScreen::new);
 		});
 	}
 }
